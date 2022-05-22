5ef6c193b5a8ea2a91a4896ae49785e9218be990
Olah Catalina <catalina.olah@wirtek.com>
2022-05-22 21:14:52 +0200

    Specify only chrome ext in allow-cors attribute

--- Data/AllowCrossSiteAttribute.cs
+++ Data/AllowCrossSiteAttribute.cs
@@ -6,8 +6,6 @@
     {
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
-            filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "null");
             filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "chrome-extension://aojklehgimofefpdekhhbagflipncinm");
             filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Headers", "*");
             filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Credentials", "true");
